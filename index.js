0d5a4e2813676fc9510635c9853e292cf7fef215
Diana Vladotsenk <DianaVladotsenk@github.noreply.github.com>
2024-09-30 11:52:51 +0300

    Ejs view engine  on kasutusuele voetud

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 const express = require("express");
+const dateTime = require("/.dateTime");
+const fs = require("fs");
+
+
 const app = express();
 
+//Maaran view mootori
+app.set("view engine", "ejs");
+//Maaran jagate avalikke failde kausta
+app.use(express.static("public"));
+
+
 app.get("/", (req,res)=>{
-	res.send("express laks kaima!");
+	//res.send("express laks kaima!");
+	res.render("index");
+});
+app.get("/timenow",(req,res)=>{
+	const weekdayEtNow  = dateTime.weekDayEt()
+	const dateEtNow = dateTime.dateFormattedEt();
+	const timeNow =  dateTime.timeFormattedEt();
+	res.render("timenow", {nowWD: weekdayEtNow, nowD: dateNow, nowT: timeNow});
+	
+});
+
+app.get("/vanas]nad", (req,res)=>{
+	let folkWisdom = [];
+	fs.readFile("public/textfiles/vanas]nad.text", "utf8", (err, data)=>{
+		if(err){
+			//throw err;
+			res.render("justlist", {h2: "Vanasõnad", listData: ["Ei leidnud midagi!"]});
+		}
+		else{
+			folkWisdmom = data.split(";");			
+			res.render("justlist", {h2: "Vanasõnad", listData: folkWisdmom});
+		}
+	});
 });
 
 app.listen(5215);
+//module.exports = {dateEtNow: dateFormattedEt,weekDayEtNow: weekDayEt, timeFormattedNow:timeFormattedEt,monthNamesEt};
+//data.split(";") - jagab ; semikoolonitele
+//forEach - kib igauht labi
---
+++ views/dateTime.js
@@ -0,0 +1,22 @@
+ const dateFormatted = function.(){
+//function dateFormatted(){
+ let timeNow = new Date();
+ let dateNow = timeNow.getDate();
+ let monthNow = timeNow.getMonth ();
+ let yearNow = timeNow.getFullYear();
+ const dayNamesEt =["pühapäev", "esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev"];
+ let dayNow = timeNow.getDay();
+ let hourNow = timeNow.getHours();
+ let secNow = timeNow.getSeconds();
+ let minuteNow = timeNow.getMinutes(); 
+ const monthNamesEt = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+//console.log("Täna on: "+ dateNow + "." + (monthNow + 1) + "." + yearNow);
+
+ //let dateEt = dateNow + "." + monthNamesEt[monthNow] + "." + yearNow + "." + dayNamesEt[dayNow] + "." + hourNow + "." + minuteNow + "." + secNow;
+ //return dateEt;
+ return dateEt = dateNow + "." + monthNamesEt[monthNow] + "." + yearNow + "." + dayNamesEt[dayNow] + "." + hourNow + "." + minuteNow + "." + secNow;
+ }
+
+module.exports = {dateEtNow: dateFormattedEt,weekDayEtNow: weekDayEt, timeFormattedNow:timeFormattedEt,monthNamesEt};
+console.log("Praegu on" + dateEt);
+//exports - ekspordib
